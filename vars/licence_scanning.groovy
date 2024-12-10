4081940fafa75fa0aab22db180874885814b4ca0
Pritam1705 <kondapratiwarpritam855@gmail.com>
2024-12-10 20:45:45 +0530

    Update licence_scanning.groovy

--- vars/licence_scanning.groovy
+++ vars/licence_scanning.groovy
@@ -2,17 +2,5 @@ def call() {
     stage('licence_scanning') {
              sh 'fossa analyze'     
     }
-    post {
-        success {
-            slackSend(channel: '#jenkinnotify', message: "Build SUCCESS: ${env.JOB_NAME} #${env.BUILD_NUMBER} (${branchName})", color: "good")
-        }
-        failure {
-            slackSend(channel: '#jenkinnotify', message: "Build FAILURE: ${env.JOB_NAME} #${env.BUILD_NUMBER} (${branchName})", color: "danger")
-        }
-        always {
-            echo "Build finished: ${currentBuild.currentResult}"
-        }
-    }
-
 
 }
