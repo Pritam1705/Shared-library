b9bda157af27acc4882e5958c2a40b28111cb0b6
Pritam1705 <kondapratiwarpritam855@gmail.com>
2024-12-11 14:22:30 +0530

    Update Push-commit.groovy

--- vars/Push-commit.groovy
+++ vars/Push-commit.groovy
@@ -1,8 +1,7 @@
 def call() {
-   
-        sh """
-            git remote set-url origin https://Pritam1705:Pritam1705@github.com/Pritam1705/salary-api.git
-            # Push the updated commit forcefully to the branch
-            git push origin main --force
-        ""
+    sh """
+        git remote set-url origin https://Pritam1705:Pritam1705@github.com/Pritam1705/salary-api.git
+        # Push the updated commit forcefully to the branch
+        git push origin main --force
+    """
 }
