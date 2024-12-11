463353aab1abc1d64fab657aa03f87cff34c2fa4
Pritam1705 <kondapratiwarpritam855@gmail.com>
2024-12-11 09:28:49 +0530

    Update Sign-off-Commit.groovy

--- vars/Sign-off-Commit.groovy
+++ vars/Sign-off-Commit.groovy
@@ -1,11 +1,11 @@
 def call() {
     
           sh '''
-                git config user.name Pritam1705
-                git config user.email kondapratiwarpritam855@gmail.com
-
-                # Amend the last commit to add a sign-off
-                git commit --amend --no-edit --signoff
-                '''
+            git config user.name "Pritam1705"
+            git config user.email "kondapratiwarpritam855@gmail.com"
+            touch file8
+            git add .
+            git commit --signoff -m "Your commit message"
+            '''
 }
 
