3b1c028a092b5e9ad251dd89881cfbe00fd916e5
Pritam1705 <kondapratiwarpritam855@gmail.com>
2024-12-11 10:04:32 +0530

    Update and rename signOffCommit.groovy to Commit.groovy

--- vars/Commit.groovy
+++ vars/Commit.groovy
@@ -3,8 +3,8 @@ def call() {
         git config --global user.name "Pritam1705"
         git config --global user.email "kondapratiwarpritam855@gmail.com"
         touch file.txt
-        git add .
-        git commit -s -m "Added with sign-off"
+        git add file.txt 
+        git commit -m "Added with sign-off"
     '''
 }
 
