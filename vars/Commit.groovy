2334754e0548759b366fd6d7374523bd0bbcf323
Pritam1705 <kondapratiwarpritam855@gmail.com>
2024-12-11 14:39:53 +0530

    Update Commit.groovy

--- vars/Commit.groovy
+++ vars/Commit.groovy
@@ -1,11 +1,18 @@
 def call() {
+    // Set up Git user details
     sh '''
         git config --global user.name "Pritam1705"
         git config --global user.email "kondapratiwarpritam855@gmail.com"
         touch file1.txt
-        git add file1.txt 
+        git add file1.txt
         git commit -m "Added with sign-off"
     '''
+    
+    // Push the changes using credentials
+    withCredentials([usernamePassword(credentialsId: 'git-cred', usernameVariable: 'GIT_USER', passwordVariable: 'GIT_PASS')]) {
+        sh """
+            git remote set-url origin https://${GIT_USER}:${GIT_PASS}@github.com/Pritam1705/salary-api.git
+            git push origin main --force
+        """
+    }
 }
-
-
