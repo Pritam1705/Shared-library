f21cc23b663215bce2c1080b6c7e9dbcbdc3672e
Pritam1705 <kondapratiwarpritam855@gmail.com>
2024-12-11 10:00:33 +0530

    Update signOffCommit.groovy

--- vars/signOffCommit.groovy
+++ vars/signOffCommit.groovy
@@ -1,14 +1,11 @@
 def call() {
-    sh """
-        git status || echo 'Git repository not initialized'
+    sh '''
         git config --global user.name "Pritam1705"
         git config --global user.email "kondapratiwarpritam855@gmail.com"
-        git status
         touch file.txt
         git add .
         git commit -s -m "Added with sign-off"
-    """
+    '''
 }
-
 
 
