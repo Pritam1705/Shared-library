21b6519e7324df7021a57fe23c176bcb38cc8ea4
Pritam1705 <kondapratiwarpritam855@gmail.com>
2024-12-16 18:43:58 +0530

    Update Initialize-Packer.groovy

--- vars/Initialize-Packer.groovy
+++ vars/Initialize-Packer.groovy
@@ -1,18 +1,18 @@
 def call() {
     script {
+        // Ensure that the AWS credentials are available
         withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', credentialsId: 'AWS-key']]) {
+            // Write the packer configuration to a file
             writeFile file: 'packer-config.pkr.hcl', text: ''' 
 packer {
-  required_plugins {
-    amazon = {
-      source  = "github.com/hashicorp/amazon"
-      version = "~> 1"
-    }
-  }
+required_plugins {
+ amazon = {
+   source  = "github.com/hashicorp/amazon"
+   version = "~> 1"
+ }
+}
 }
 '''
         }
     }
-}
-  
 }
