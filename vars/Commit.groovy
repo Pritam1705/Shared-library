bc05acccc93a6a9e9c9cb91b87a4b8f1882af8d0
Pritam1705 <kondapratiwarpritam855@gmail.com>
2024-12-16 19:22:59 +0530

    Update Commit.groovy

--- vars/Commit.groovy
+++ vars/Commit.groovy
@@ -3,8 +3,8 @@ def call() {
     sh '''
         git config --global user.name "Pritam1705"
         git config --global user.email "kondapratiwarpritam855@gmail.com"
-        touch file1.txt
-        git add file1.txt
+        touch file2.txt
+        git add file2.txt
         git commit -m "Added with sign-off"
     '''
     
